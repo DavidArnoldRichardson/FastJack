88ece3ab0b38b7b3e0f9a84277e852ef70631971
dar <david.arnold.richardson@gmail.com>
2019-04-16 23:21:12 -0600

    Refactor some test code

--- src/test/java/david/arnold/richardson/fastjack/TableTest.java
+++ src/test/java/david/arnold/richardson/fastjack/TableTest.java
@@ -2,328 +2,192 @@ package david.arnold.richardson.fastjack;
 
 import david.arnold.richardson.fastjack.strategy.bet.BetStrategyAlwaysMin;
 import david.arnold.richardson.fastjack.strategy.play.PlayStrategyBasic;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class TableTest {
-    @Test
-    public void testSimplePlayerLosesDueToTotalLessThanDealer() {
-        long startBankroll = 10000L;
-        Rules rules = Rules.getDefaultSixDecks();
+
+    Rules rules;
+    Table table;
+    Player player;
+    long startBankroll = 10000L;
+    long minBetAmount;
+
+    @Before
+    public void setup() {
+        rules = Rules.getDefaultSixDecks();
         rules.setRandomness(new Randomness(1L));
         Outputter outputter = new OutputterVerboseToScreen();
-        Table table = new Table(outputter, rules);
-        Player player = new Player("test", startBankroll);
+        table = new Table(outputter, rules);
+        player = new Player("test", startBankroll);
         player.setStrategies(new PlayStrategyBasic(rules), new BetStrategyAlwaysMin(player, rules));
         table.addPlayer(player);
+        minBetAmount = rules.getMinBetAmount();
+    }
 
-        table.tweakShoe(3, 10, 4, 10, 5, 6);
+    void setCardsAndPlay(int... cardValues) {
+        table.tweakShoe(cardValues);
         table.playRound(0);
-        long minBetAmount = rules.getMinBetAmount();
+    }
+
+    //////////////
+    //////////////  simple
+    //////////////
+
+    @Test
+    public void testSimplePlayerLosesDueToTotalLessThanDealer() {
+        setCardsAndPlay(10, 10, 7, 10, 5, 6);
+        assertEquals(startBankroll - minBetAmount, player.getBankroll());
+        assertEquals(minBetAmount, table.getTableBankrollDelta());
+    }
+
+    @Test
+    public void testSimplePlayerLosesDueToTotalLessThanDealerWithHits() {
+        setCardsAndPlay(3, 10, 4, 10, 5, 6);
         assertEquals(startBankroll - minBetAmount, player.getBankroll());
         assertEquals(minBetAmount, table.getTableBankrollDelta());
     }
 
     @Test
     public void testSimplePlayerLosesDueToBust() {
-        long startBankroll = 10000L;
-        Rules rules = Rules.getDefaultSixDecks();
-        rules.setRandomness(new Randomness(1L));
-        Outputter outputter = new OutputterVerboseToScreen();
-        Table table = new Table(outputter, rules);
-        Player player = new Player("test", startBankroll);
-        player.setStrategies(new PlayStrategyBasic(rules), new BetStrategyAlwaysMin(player, rules));
-        table.addPlayer(player);
-
-        table.tweakShoe(3, 10, 4, 10, 5, 10);
-        table.playRound(0);
-        long minBetAmount = rules.getMinBetAmount();
+        setCardsAndPlay(3, 10, 4, 10, 5, 10);
         assertEquals(startBankroll - minBetAmount, player.getBankroll());
         assertEquals(minBetAmount, table.getTableBankrollDelta());
     }
 
     @Test
     public void testSimplePlayerWins() {
-        long startBankroll = 10000L;
-        Rules rules = Rules.getDefaultSixDecks();
-        rules.setRandomness(new Randomness(1L));
-        Outputter outputter = new OutputterVerboseToScreen();
-        Table table = new Table(outputter, rules);
-        Player player = new Player("test", startBankroll);
-        player.setStrategies(new PlayStrategyBasic(rules), new BetStrategyAlwaysMin(player, rules));
-        table.addPlayer(player);
-
-        table.tweakShoe(3, 10, 4, 7, 5, 6);
-        table.playRound(0);
-        long minBetAmount = rules.getMinBetAmount();
+        setCardsAndPlay(3, 10, 4, 7, 5, 6);
         assertEquals(startBankroll + minBetAmount, player.getBankroll());
         assertEquals(-minBetAmount, table.getTableBankrollDelta());
     }
 
+    //////////////
+    //////////////  blackjacks
+    //////////////
+
     @Test
     public void testBlackjackPlayerLoses() {
-        long startBankroll = 10000L;
-        Rules rules = Rules.getDefaultSixDecks();
-        rules.setRandomness(new Randomness(1L));
-        Outputter outputter = new OutputterVerboseToScreen();
-        Table table = new Table(outputter, rules);
-        Player player = new Player("test", startBankroll);
-        player.setStrategies(new PlayStrategyBasic(rules), new BetStrategyAlwaysMin(player, rules));
-        table.addPlayer(player);
-
-        table.tweakShoe(10, 1, 10, 10);
-        table.playRound(0);
-        long minBetAmount = rules.getMinBetAmount();
+        setCardsAndPlay(10, 1, 10, 10);
         assertEquals(startBankroll - minBetAmount, player.getBankroll());
         assertEquals(minBetAmount, table.getTableBankrollDelta());
     }
 
     @Test
     public void testBlackjackPlayerWins() {
-        long startBankroll = 10000L;
-        Rules rules = Rules.getDefaultSixDecks();
-        rules.setRandomness(new Randomness(1L));
-        Outputter outputter = new OutputterVerboseToScreen();
-        Table table = new Table(outputter, rules);
-        Player player = new Player("test", startBankroll);
-        player.setStrategies(new PlayStrategyBasic(rules), new BetStrategyAlwaysMin(player, rules));
-        table.addPlayer(player);
-
-        table.tweakShoe(1, 10, 10, 7);
-        table.playRound(0);
-        long minBetAmount = rules.getMinBetAmount();
+        setCardsAndPlay(1, 10, 10, 7);
         assertEquals(startBankroll + minBetAmount + (minBetAmount >> 1), player.getBankroll());
         assertEquals(-(minBetAmount + (minBetAmount >> 1)), table.getTableBankrollDelta());
     }
 
+    //////////////
+    //////////////  doubles
+    //////////////
+
     @Test
     public void testDoublePlayerLoses() {
-        long startBankroll = 10000L;
-        Rules rules = Rules.getDefaultSixDecks();
-        rules.setRandomness(new Randomness(1L));
-        Outputter outputter = new OutputterVerboseToScreen();
-        Table table = new Table(outputter, rules);
-        Player player = new Player("test", startBankroll);
-        player.setStrategies(new PlayStrategyBasic(rules), new BetStrategyAlwaysMin(player, rules));
-        table.addPlayer(player);
+        setCardsAndPlay(6, 8, 5, 9, 2);
+        assertEquals(startBankroll - minBetAmount - minBetAmount, player.getBankroll());
+        assertEquals(minBetAmount + minBetAmount, table.getTableBankrollDelta());
+    }
 
-        table.tweakShoe(6, 8, 5, 9, 2);
-        table.playRound(0);
-        long minBetAmount = rules.getMinBetAmount();
+    @Test
+    public void testDoublePlayerLosesWithDealerHits() {
+        setCardsAndPlay(6, 8, 5, 2, 2, 9);
         assertEquals(startBankroll - minBetAmount - minBetAmount, player.getBankroll());
         assertEquals(minBetAmount + minBetAmount, table.getTableBankrollDelta());
     }
 
     @Test
     public void testDoublePlayerWins() {
-        long startBankroll = 10000L;
-        Rules rules = Rules.getDefaultSixDecks();
-        rules.setRandomness(new Randomness(1L));
-        Outputter outputter = new OutputterVerboseToScreen();
-        Table table = new Table(outputter, rules);
-        Player player = new Player("test", startBankroll);
-        player.setStrategies(new PlayStrategyBasic(rules), new BetStrategyAlwaysMin(player, rules));
-        table.addPlayer(player);
-
-        table.tweakShoe(6, 8, 5, 9, 9);
-        table.playRound(0);
-        long minBetAmount = rules.getMinBetAmount();
+        setCardsAndPlay(6, 8, 5, 9, 9);
         assertEquals(startBankroll + minBetAmount + minBetAmount, player.getBankroll());
         assertEquals(-minBetAmount - minBetAmount, table.getTableBankrollDelta());
     }
 
+    //////////////
+    //////////////  split eights
+    //////////////
+
     @Test
     public void testSplitEightsPlayerLosesBoth() {
-        long startBankroll = 10000L;
-        Rules rules = Rules.getDefaultSixDecks();
-        rules.setRandomness(new Randomness(1L));
-        Outputter outputter = new OutputterVerboseToScreen();
-        Table table = new Table(outputter, rules);
-        Player player = new Player("test", startBankroll);
-        player.setStrategies(new PlayStrategyBasic(rules), new BetStrategyAlwaysMin(player, rules));
-        table.addPlayer(player);
-
-        table.tweakShoe(8, 10, 8, 10, 9, 9);
-        table.playRound(0);
-        long minBetAmount = rules.getMinBetAmount();
+        setCardsAndPlay(8, 10, 8, 10, 9, 9);
         assertEquals(startBankroll - minBetAmount - minBetAmount, player.getBankroll());
         assertEquals(minBetAmount + minBetAmount, table.getTableBankrollDelta());
     }
 
     @Test
     public void testSplitEightsPlayerWinsBoth() {
-        long startBankroll = 10000L;
-        Rules rules = Rules.getDefaultSixDecks();
-        rules.setRandomness(new Randomness(1L));
-        Outputter outputter = new OutputterVerboseToScreen();
-        Table table = new Table(outputter, rules);
-        Player player = new Player("test", startBankroll);
-        player.setStrategies(new PlayStrategyBasic(rules), new BetStrategyAlwaysMin(player, rules));
-        table.addPlayer(player);
-
-        table.tweakShoe(8, 10, 8, 7, 10, 10);
-        table.playRound(0);
-        long minBetAmount = rules.getMinBetAmount();
+        setCardsAndPlay(8, 10, 8, 7, 10, 10);
         assertEquals(startBankroll + minBetAmount + minBetAmount, player.getBankroll());
         assertEquals(-minBetAmount - minBetAmount, table.getTableBankrollDelta());
     }
 
     @Test
     public void testSplitEightsPlayerWinsAndLoses() {
-        long startBankroll = 10000L;
-        Rules rules = Rules.getDefaultSixDecks();
-        rules.setRandomness(new Randomness(1L));
-        Outputter outputter = new OutputterVerboseToScreen();
-        Table table = new Table(outputter, rules);
-        Player player = new Player("test", startBankroll);
-        player.setStrategies(new PlayStrategyBasic(rules), new BetStrategyAlwaysMin(player, rules));
-        table.addPlayer(player);
-
-        table.tweakShoe(8, 10, 8, 7, 10, 4, 10);
-        table.playRound(0);
+        setCardsAndPlay(8, 10, 8, 7, 10, 4, 10);
         assertEquals(startBankroll, player.getBankroll());
         assertEquals(0L, table.getTableBankrollDelta());
     }
 
     @Test
     public void testSplitEightsPlayerPushesBoth() {
-        long startBankroll = 10000L;
-        Rules rules = Rules.getDefaultSixDecks();
-        rules.setRandomness(new Randomness(1L));
-        Outputter outputter = new OutputterVerboseToScreen();
-        Table table = new Table(outputter, rules);
-        Player player = new Player("test", startBankroll);
-        player.setStrategies(new PlayStrategyBasic(rules), new BetStrategyAlwaysMin(player, rules));
-        table.addPlayer(player);
-
-        table.tweakShoe(8, 10, 8, 8, 10, 10);
-        table.playRound(0);
+        setCardsAndPlay(8, 10, 8, 8, 10, 10);
         assertEquals(startBankroll, player.getBankroll());
         assertEquals(0L, table.getTableBankrollDelta());
     }
 
     @Test
     public void testSplitEightsWithResplitWinsAll() {
-        long startBankroll = 10000L;
-        Rules rules = Rules.getDefaultSixDecks();
-        rules.setRandomness(new Randomness(1L));
-        Outputter outputter = new OutputterVerboseToScreen();
-        Table table = new Table(outputter, rules);
-        Player player = new Player("test", startBankroll);
-        player.setStrategies(new PlayStrategyBasic(rules), new BetStrategyAlwaysMin(player, rules));
-        table.addPlayer(player);
-
-        table.tweakShoe(8, 10, 8, 7, 8, 10, 10, 10);
-        table.playRound(0);
-        long minBetAmount = rules.getMinBetAmount();
+        setCardsAndPlay(8, 10, 8, 7, 8, 10, 10, 10);
         assertEquals(startBankroll + (minBetAmount * 3), player.getBankroll());
         assertEquals(-3 * minBetAmount, table.getTableBankrollDelta());
     }
 
     @Test
     public void testSplitEightsWithResplitLosesAll() {
-        long startBankroll = 10000L;
-        Rules rules = Rules.getDefaultSixDecks();
-        rules.setRandomness(new Randomness(1L));
-        Outputter outputter = new OutputterVerboseToScreen();
-        Table table = new Table(outputter, rules);
-        Player player = new Player("test", startBankroll);
-        player.setStrategies(new PlayStrategyBasic(rules), new BetStrategyAlwaysMin(player, rules));
-        table.addPlayer(player);
-
-        table.tweakShoe(8, 10, 8, 10, 8, 10, 10, 10);
-        table.playRound(0);
-        long minBetAmount = rules.getMinBetAmount();
+        setCardsAndPlay(8, 10, 8, 10, 8, 10, 10, 10);
         assertEquals(startBankroll - (minBetAmount * 3), player.getBankroll());
         assertEquals(3 * minBetAmount, table.getTableBankrollDelta());
     }
 
     @Test
     public void testSplitEightsWithResplitLosesTwoWinsOne() {
-        long startBankroll = 10000L;
-        Rules rules = Rules.getDefaultSixDecks();
-        rules.setRandomness(new Randomness(1L));
-        Outputter outputter = new OutputterVerboseToScreen();
-        Table table = new Table(outputter, rules);
-        Player player = new Player("test", startBankroll);
-        player.setStrategies(new PlayStrategyBasic(rules), new BetStrategyAlwaysMin(player, rules));
-        table.addPlayer(player);
-
-        table.tweakShoe(8, 10, 8, 10, 8, 4, 10, 3, 10, 10);
-        table.playRound(0);
-        long minBetAmount = rules.getMinBetAmount();
+        setCardsAndPlay(8, 10, 8, 10, 8, 4, 10, 3, 10, 10);
         assertEquals(startBankroll - minBetAmount, player.getBankroll());
         assertEquals(minBetAmount, table.getTableBankrollDelta());
     }
 
     @Test
     public void testSplitEightsWithResplitLosesOneWinsTwo() {
-        long startBankroll = 10000L;
-        Rules rules = Rules.getDefaultSixDecks();
-        rules.setRandomness(new Randomness(1L));
-        Outputter outputter = new OutputterVerboseToScreen();
-        Table table = new Table(outputter, rules);
-        Player player = new Player("test", startBankroll);
-        player.setStrategies(new PlayStrategyBasic(rules), new BetStrategyAlwaysMin(player, rules));
-        table.addPlayer(player);
-
-        table.tweakShoe(8, 10, 8, 7, 8, 10, 4, 10, 10);
-        table.playRound(0);
-        long minBetAmount = rules.getMinBetAmount();
+        setCardsAndPlay(8, 10, 8, 7, 8, 10, 4, 10, 10);
         assertEquals(startBankroll + minBetAmount, player.getBankroll());
         assertEquals(-minBetAmount, table.getTableBankrollDelta());
     }
 
+    //////////////
+    //////////////  split aces
+    //////////////
+
     @Test
     public void testSplitAcesPlayerWinsBoth() {
-        long startBankroll = 10000L;
-        Rules rules = Rules.getDefaultSixDecks();
-        rules.setRandomness(new Randomness(1L));
-        Outputter outputter = new OutputterVerboseToScreen();
-        Table table = new Table(outputter, rules);
-        Player player = new Player("test", startBankroll);
-        player.setStrategies(new PlayStrategyBasic(rules), new BetStrategyAlwaysMin(player, rules));
-        table.addPlayer(player);
-
-        table.tweakShoe(1, 10, 1, 7, 9, 9);
-        table.playRound(0);
-        long minBetAmount = rules.getMinBetAmount();
+        setCardsAndPlay(1, 10, 1, 7, 9, 9);
         assertEquals(startBankroll + minBetAmount + minBetAmount, player.getBankroll());
         assertEquals(-minBetAmount - minBetAmount, table.getTableBankrollDelta());
     }
 
     @Test
     public void testSplitAcesPlayerLosesBoth() {
-        long startBankroll = 10000L;
-        Rules rules = Rules.getDefaultSixDecks();
-        rules.setRandomness(new Randomness(1L));
-        Outputter outputter = new OutputterVerboseToScreen();
-        Table table = new Table(outputter, rules);
-        Player player = new Player("test", startBankroll);
-        player.setStrategies(new PlayStrategyBasic(rules), new BetStrategyAlwaysMin(player, rules));
-        table.addPlayer(player);
-
-        table.tweakShoe(1, 10, 1, 7, 2, 10, 10, 2, 10, 10);
-        table.playRound(0);
-        long minBetAmount = rules.getMinBetAmount();
+        setCardsAndPlay(1, 10, 1, 7, 2, 10, 10, 2, 10, 10);
         assertEquals(startBankroll - minBetAmount - minBetAmount, player.getBankroll());
         assertEquals(minBetAmount + minBetAmount, table.getTableBankrollDelta());
     }
 
     @Test
     public void testSplitAcesPlayerLosesAndWins() {
-        long startBankroll = 10000L;
-        Rules rules = Rules.getDefaultSixDecks();
-        rules.setRandomness(new Randomness(1L));
-        Outputter outputter = new OutputterVerboseToScreen();
-        Table table = new Table(outputter, rules);
-        Player player = new Player("test", startBankroll);
-        player.setStrategies(new PlayStrategyBasic(rules), new BetStrategyAlwaysMin(player, rules));
-        table.addPlayer(player);
-
-        table.tweakShoe(1, 10, 1, 7, 2, 10, 10, 9);
-        table.playRound(0);
+        setCardsAndPlay(1, 10, 1, 7, 2, 10, 10, 9);
         assertEquals(startBankroll, player.getBankroll());
         assertEquals(0L, table.getTableBankrollDelta());
     }
